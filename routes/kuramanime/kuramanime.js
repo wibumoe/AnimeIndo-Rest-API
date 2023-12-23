8d77d396e48d59c3de36fa40f03bc766123d6cbb
Wibumoe <131719695+wibumoe@users.noreply.github.com>
2023-12-24 02:51:06 +0700

    Update kuramanime.js

--- routes/kuramanime/kuramanime.js
+++ routes/kuramanime/kuramanime.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const {
+  getOngoingAnime,
+  getScheeduleAnime,
+  getPropertiesAnime,
+  getPropertiesGenre,
+  getDetailsAnime,
+  getSearchAnime,
   getRecentRelease,
   getAnimeEpisode,
 } = require("./parser");
@@ -10,7 +16,13 @@ router.get("/", function (req, res) {
      message: "Welcome to the LuckyAnime API!",
   });
 });
+router.get("/ongoing", getOngoingAnime);
+router.get("/schedule", getScheeduleAnime);
+router.get("/properties", getPropertiesAnime);
+router.get("/properties/genre/:genreName", getPropertiesGenre);
 router.get("/details/anime/:animeId/:animeIdTitle", getDetailsAnime);
+router.get("/search", getSearchAnime);
+router.get("/recent", getRecentRelease);
 router.get(
   "/watch/anime/:animeName/:animeId/episode/:episodeId",
   getAnimeEpisode
